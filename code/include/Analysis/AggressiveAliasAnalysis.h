0ddde35ac63f3a065560013fbba98bdc64f20e47
Devajith <Devajith Valaparambil Sreeramaswamy>
2025-03-05 20:27:16 +0100

    Revert "transfer function for store"
    This reverts commit 776593fb37fefe3bfbe0685a70e25aa889c0eec4.

--- code/include/Analysis/AggressiveAliasAnalysis.h
+++ code/include/Analysis/AggressiveAliasAnalysis.h
@@ -111,7 +111,6 @@ public:
   AliasRelation replaceWith(mlir::Value, mlir::Value);
 
   mlir::Value getLeft() const;
-  mlir::Value getRight() const;
 
 private:
   mlir::Value left;
@@ -148,12 +147,6 @@ class CallSite {
 
 public:
   Function *getFun() const { return fun; }
-
-  bool isNull() const;
-
-  friend bool operator==(const CallSite &l, const CallSite &r) {
-    return l.fun == r.fun;
-  }
 };
 
 /// Alias instance (AI)
@@ -164,18 +157,6 @@ class AliasInstance {
 
 public:
   Function *getFun() const { return c.getFun(); }
-
-  /// AliasRelation
-  void setAliasRelation(const AliasRelation &);
-  AliasRelation getAliasRelation() const;
-
-  /// AliasSet
-  void setAliasSet(const AliasSet &);
-  AliasSet getAliasSet() const;
-
-  // CallSite
-  void setCallSite(const CallSite &);
-  CallSite getCallSite() const;
 };
 
 /// Alias instance set
@@ -188,12 +169,6 @@ public:
 
   std::vector<AliasInstance> getSets() const;
 
-  AliasInstanceSet join(const AliasInstanceSet &);
-
-  std::vector<AliasInstance> getSetsWhereAliasRelationLeftIs(mlir::Value);
-
-  void insert(const AliasInstance &);
-
 private:
   std::set<AliasInstance> mayAliases;
 };
@@ -203,7 +178,7 @@ public:
   AliasInstanceSet getEntryAliasSet();
   AliasInstanceSet getExitAliasSet();
 
-  void setExitSet(const AliasInstanceSet &);
+  void setExitSet(const AliasSet &);
 
   mlir::func::FuncOp getFuncOp();
 
