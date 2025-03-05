b567e5e47ce4049f9dd7cf10ec2d7c43c8b99a68
Devajith <Devajith Valaparambil Sreeramaswamy>
2025-03-05 22:24:40 +0100

    Remove final compile errors

---
+++ code/include/Mir/MirOpsInterfaces.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "MirAttr.h"
+#include "MirTypes.h"
+
+#include <mlir/IR/BuiltinTypes.h>
+#include <mlir/IR/OpDefinition.h>
+
+namespace rust_compiler::Mir {
+
+
+} // namespace rust_compiler::Mir
+
+#include "MirOpsInterfaces.h.inc"
